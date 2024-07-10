a107a5fd227c5bb234a5ec228b43f35f7b25d031
Ed Bennett <6391276+edwardjosephbennett@users.noreply.github.com>
2024-07-10 18:06:06 +0100

    chore(init): first commit

---
+++ code-standards/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'code-standards',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/code-standards',
+};
