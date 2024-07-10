5839f65904c4d7cd678233674931caf82e23e191
Ed Bennett <6391276+edwardjosephbennett@users.noreply.github.com>
2024-07-10 18:46:46 +0100

    feat(config): add some new console logs

--- jh-code-standards/index.js
+++ jh-code-standards/index.js
@@ -3,3 +3,8 @@ console.log("Hello World 2");
 console.log('Hello World 3');
 console.log("Hello World 4");
 console.log('Hello World 5');
+console.log("Hello World 6");
+console.log('Hello World 7');
+console.log("Hello World 8");
+console.log('Hello World 9');
+console.log("Hello World 10");
