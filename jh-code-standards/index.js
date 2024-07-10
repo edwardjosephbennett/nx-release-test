ed519e24d3ac0eea9b87678680b508a67f719656
Ed Bennett <6391276+edwardjosephbennett@users.noreply.github.com>
2024-07-10 19:00:55 +0100

    feat(lib): remove some console logs

--- jh-code-standards/index.js
+++ jh-code-standards/index.js
@@ -1,10 +1,2 @@
 console.log('Hello World');
 console.log("Hello World 2");
-console.log('Hello World 3');
-console.log("Hello World 4");
-console.log('Hello World 5');
-console.log("Hello World 6");
-console.log('Hello World 7');
-console.log("Hello World 8");
-console.log('Hello World 9');
-console.log("Hello World 10");
