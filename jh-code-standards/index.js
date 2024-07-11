c5f37f0c8cd5a165496725b910db3b0ec561717c
Ed Bennett <6391276+edwardjosephbennett@users.noreply.github.com>
2024-07-11 15:11:17 +0100

    feat(lib): dump some console logs

--- jh-code-standards/index.js
+++ jh-code-standards/index.js
@@ -1,7 +1,1 @@
 console.log('Hello World');
-console.log("Hello World 2");
-console.log("Hello World 3");
-console.log("Hello World 4");
-console.log("Hello World 5");
-console.log("Hello World 6");
-console.log("Hello World 7");
